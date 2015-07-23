4769227c5e532779010f80fa6ce173bb8e120902
Jorge Godinho <jorge.godinho@gmail.com>
2015-07-23 17:05:47 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -178,7 +178,7 @@
 }
 
 -(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair*)pair character:(CCSprite*)character level:(CCNode*)level {
-    //[self gameOver];
+    [self gameOver];
     return TRUE;
 }
 
